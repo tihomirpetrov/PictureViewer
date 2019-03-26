cb90ee168f64d3987cf90cae5fe68d2d772d9f8a
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-03-26 12:54:16 +0200

    initial commit

---
+++ VideoPlayerWithDirectX/VideoPlayer/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace VideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnPlayPause = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnFullscreen = new System.Windows.Forms.Button();
+            this.btnVolume = new System.Windows.Forms.Button();
+            this.pnlVideo = new System.Windows.Forms.Panel();
+            this.lstVideos = new System.Windows.Forms.ListBox();
+            this.tmrVideo = new System.Windows.Forms.Timer(this.components);
+            this.trackVolume = new System.Windows.Forms.TrackBar();
+            this.lblVideo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(13, 403);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 0;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.Location = new System.Drawing.Point(94, 403);
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayPause.TabIndex = 1;
+            this.btnPlayPause.Text = "Play/Pause";
+            this.btnPlayPause.UseVisualStyleBackColor = true;
+            this.btnPlayPause.Click += new System.EventHandler(this.btnPlayPause_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(175, 403);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnFullscreen
+            // 
+            this.btnFullscreen.Location = new System.Drawing.Point(256, 403);
+            this.btnFullscreen.Name = "btnFullscreen";
+            this.btnFullscreen.Size = new System.Drawing.Size(75, 23);
+            this.btnFullscreen.TabIndex = 3;
+            this.btnFullscreen.Text = "Fullscreen";
+            this.btnFullscreen.UseVisualStyleBackColor = true;
+            this.btnFullscreen.Click += new System.EventHandler(this.btnFullscreen_Click);
+            // 
+            // btnVolume
+            // 
+            this.btnVolume.Location = new System.Drawing.Point(337, 403);
+            this.btnVolume.Name = "btnVolume";
+            this.btnVolume.Size = new System.Drawing.Size(75, 23);
+            this.btnVolume.TabIndex = 4;
+            this.btnVolume.Text = "Volume";
+            this.btnVolume.UseVisualStyleBackColor = true;
+            this.btnVolume.Click += new System.EventHandler(this.btnVolume_Click);
+            // 
+            // pnlVideo
+            // 
+            this.pnlVideo.Location = new System.Drawing.Point(13, 13);
+            this.pnlVideo.Name = "pnlVideo";
+            this.pnlVideo.Size = new System.Drawing.Size(631, 372);
+            this.pnlVideo.TabIndex = 5;
+            // 
+            // lstVideos
+            // 
+            this.lstVideos.FormattingEnabled = true;
+            this.lstVideos.Location = new System.Drawing.Point(662, 13);
+            this.lstVideos.Name = "lstVideos";
+            this.lstVideos.Size = new System.Drawing.Size(210, 368);
+            this.lstVideos.TabIndex = 6;
+            this.lstVideos.SelectedIndexChanged += new System.EventHandler(this.lstVideos_SelectedIndexChanged);
+            // 
+            // tmrVideo
+            // 
+            this.tmrVideo.Tick += new System.EventHandler(this.tmrVideo_Tick);
+            // 
+            // trackVolume
+            // 
+            this.trackVolume.Location = new System.Drawing.Point(418, 403);
+            this.trackVolume.Maximum = 0;
+            this.trackVolume.Minimum = -10000;
+            this.trackVolume.Name = "trackVolume";
+            this.trackVolume.Size = new System.Drawing.Size(107, 45);
+            this.trackVolume.TabIndex = 7;
+            this.trackVolume.Scroll += new System.EventHandler(this.trackVolume_Scroll);
+            // 
+            // lblVideo
+            // 
+            this.lblVideo.AutoSize = true;
+            this.lblVideo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblVideo.Location = new System.Drawing.Point(175, 474);
+            this.lblVideo.Name = "lblVideo";
+            this.lblVideo.Size = new System.Drawing.Size(71, 19);
+            this.lblVideo.TabIndex = 8;
+            this.lblVideo.Text = "lblVideo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 544);
+            this.Controls.Add(this.lblVideo);
+            this.Controls.Add(this.trackVolume);
+            this.Controls.Add(this.lstVideos);
+            this.Controls.Add(this.pnlVideo);
+            this.Controls.Add(this.btnVolume);
+            this.Controls.Add(this.btnFullscreen);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPlayPause);
+            this.Controls.Add(this.btnPrevious);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnPlayPause;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnFullscreen;
+        private System.Windows.Forms.Button btnVolume;
+        private System.Windows.Forms.Panel pnlVideo;
+        private System.Windows.Forms.ListBox lstVideos;
+        private System.Windows.Forms.Timer tmrVideo;
+        private System.Windows.Forms.TrackBar trackVolume;
+        private System.Windows.Forms.Label lblVideo;
+    }
+}
+
